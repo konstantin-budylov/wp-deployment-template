21a4c14e0f6d52460c062e8c935003eee8bb01d5
Konstantin Budylov <konst19676b@gmail.com>
2025-10-30 18:03:43 +0530

    feat(deployment): setup wordpress deployment

--- deployment/services/mysql/init/01-init.sql
+++ deployment/services/mysql/init/01-init.sql
@@ -1,12 +1,12 @@
 -- MySQL initialization script
 -- Create databases
-CREATE DATABASE IF NOT EXISTS test CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS wordpress CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 -- Create test user with password
-CREATE USER IF NOT EXISTS 'test'@'%' IDENTIFIED BY 'test123';
+CREATE USER IF NOT EXISTS 'wp'@'%' IDENTIFIED BY 'wp_pass';
 
 -- Grant privileges to test user
-GRANT ALL PRIVILEGES ON test.* TO 'test'@'%';
+GRANT ALL PRIVILEGES ON wordpress.* TO 'wp'@'%';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
